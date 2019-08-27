59ce19bc2a96f04eaee6ca24d420c22c75a04523
outsky <317525370@qq.com>
2019-08-27 13:28:57 +0800

    makefile

---
+++ autogenmk.sh
@@ -0,0 +1,31 @@
+#/bin/sh
+
+set -e
+set -u
+
+ALL_O=""
+CC="cc"
+
+OBJS="# autogen with $CC -MM"
+for t in *.c; do
+    ALL_O=$ALL_O${t%%.*}.o" "
+    OBJS=$OBJS$(echo -e "\n"$($CC -MM $t))
+done
+
+echo "# Autogened at `date +\"%Y/%m/%d %H:%M:%S\"`
+
+BIN = luna
+
+CFLAGS = -g Wall -std=c99 -D_GNU_SOURCE
+
+LIBS = #empty
+
+ALL_O = $ALL_O
+
+\$(BIN): \$(ALL_O)
+	$CC -o \$@ \$(CFLAGS) \$(ALL_O) \$(LIBS)
+
+clean:
+	rm -f \$(BIN) \$(ALL_O)
+
+$OBJS" > makefile
---
+++ makefile
@@ -0,0 +1,21 @@
+# Autogened at 2019/08/27 13:28:10
+
+BIN = luna
+
+CFLAGS = -g Wall -std=c99 -D_GNU_SOURCE
+
+LIBS = #empty
+
+ALL_O = lasm.o lib.o list.o main.o 
+
+$(BIN): $(ALL_O)
+	cc -o $@ $(CFLAGS) $(ALL_O) $(LIBS)
+
+clean:
+	rm -f $(BIN) $(ALL_O)
+
+# autogen with cc -MM
+lasm.o: lasm.c
+lib.o: lib.c lib.h
+list.o: list.c list.h
+main.o: main.c lib.h
